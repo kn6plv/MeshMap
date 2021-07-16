43040b6c4c8744bf085293fdf89fd57b088db940
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2021-07-16 14:09:57 -0700

    Ope info box from links

--- src/components/BaArednMap.jsx
+++ src/components/BaArednMap.jsx
@@ -31,78 +31,87 @@ const GrayIcon = new Icon({
 
 // Function to get the Freq Icon
 function getIcon(freq){
-  if(freq !== null && typeof freq !== 'undefined' ) 
+  if(freq !== null && typeof freq !== 'undefined' )
   {
     if(freq.includes("2.")) {
-      return PurpleIcon
+      return PurpleIcon;
     }
     else if(freq.includes("5.")){
-      return OrangeIcon 
+      return OrangeIcon;
     }
     else if(freq.includes("3.")){
-      return BlueIcon
+      return BlueIcon;
     }
     else if (freq.includes("900")){
-      return MagentaIcon
+      return MagentaIcon;
     }
   }
-  return GrayIcon
-  
+  return GrayIcon;
 }
 
 class BaArednMap extends Component {
 
-
-state = {
+  state = {
       //appConfig: this.props.appConfig,
       zoom: 9.5,
       mapCenter: {
         lat: 18.2,
         lon: -66.3,
-      },
-      
+      }
   }
 
   componentDidMount() {
     //this.setState({appConfig: this.props.appConfig})
     //console.log(this.state,"ACTUAL STATE")
-
-    }
+  }
 
   render() {
     //console.log(this.props.appConfig,"appConfig")
-    if(this.props.appConfig.length === 0) { return null }
+    if(this.props.appConfig.length === 0) {
+      return null;
+    }
     else {
-    //const mapCenter = [this.state.mapCenter.lat, this.state.mapCenter.lon];
-    const mapCenter = [this.props.appConfig.mapSettings.mapCenter.lat, this.props.appConfig.mapSettings.mapCenter.lon];
-    return (
-      <Map className="Map" center={mapCenter} zoom={this.props.appConfig.mapSettings.zoom} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-          { this.props.nodesData.map(n => <Marker key={n.node} position={[n.lat,n.lon]} icon={ getIcon(n.meshrf.freq) }>
-            <Popup>  
-              {<div><h6><a href={`http://${n.node}.local.mesh`} target="_blank">{n.node}</a></h6>
-              <p>
-                 Desc: {n.node_details.description} <br/>
-                 Position: ({n.lat},{n.lon})<br/>
-                 RF Status: {n.meshrf.status}<br/>
-                 SSID: {n.meshrf.ssid} <br/>
-                 RF Channel: {n.meshrf.channel}<br/>
-                 RF Freq: {n.meshrf.freq}<br/>
-                 MAC: {n.interfaces[0].mac}<br/>
-                 Model: {n.node_details.model}<br/>
-                 Firmware Ver: {n.node_details.firmware_version} <br/> </p>            
-                 Neighbors:<br/><ul> { n.link_info.map(m => <li key={m.hostname}><a href={`http://${m.hostname}`} target="_blank">{m.hostname.replace(/\.local\.mesh$/,'')}</a> { m.linkType ? `(${m.linkType})` : "" } </li>)} </ul> </div>
-                }      
-            </Popup>
-          </Marker>)}
-          
-      </Map>
-    );
+      //const mapCenter = [this.state.mapCenter.lat, this.state.mapCenter.lon];
+      const mapCenter = [this.props.appConfig.mapSettings.mapCenter.lat, this.props.appConfig.mapSettings.mapCenter.lon];
+      return (
+        <Map ref="map" className="Map" center={mapCenter} zoom={this.props.appConfig.mapSettings.zoom} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+            { this.props.nodesData.map(n =>
+              <Marker ref={n.node} key={n.node} position={[n.lat,n.lon]} icon={ getIcon(n.meshrf.freq) }>
+                <Popup> {
+                  <div><h6><a href={`http://${n.node}.local.mesh`} target="_blank">{n.node}</a></h6>
+                  <p>
+                    Desc: {n.node_details.description}<br/>
+                    Position: ({n.lat},{n.lon})<br/>
+                    RF Status: {n.meshrf.status}<br/>
+                    SSID: {n.meshrf.ssid}<br/>
+                    RF Channel: {n.meshrf.channel}<br/>
+                    RF Freq: {n.meshrf.freq}<br/>
+                    MAC: {n.interfaces[0].mac}<br/>
+                    Model: {n.node_details.model}<br/>
+                    Firmware Ver: {n.node_details.firmware_version}<br/>
+                    </p>
+                    Neighbors:<br/><ul> {
+                      n.link_info.map(m => <li key={m.hostname}><a href="#" onClick={()=>this.openPopup(m.hostname.replace(/\.local\.mesh$/,''))}>{m.hostname.replace(/\.local\.mesh$/,'')}</a> { m.linkType ? `(${m.linkType})` : "" } </li>)
+                    } </ul>
+                  </div>
+                }
+              </Popup>
+            </Marker>)}
+        </Map>
+      );
+    }
   }
-}
+
+  openPopup(id) {
+    const popup = this.refs[id];
+    if (popup) {
+      popup.fireLeafletEvent('click');
+    }
+  }
 }
 
 export default BaArednMap;
