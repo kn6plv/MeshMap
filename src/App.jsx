7ca8851a1a33ca515428bedee0a8c9247a20d845
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2021-07-17 16:31:56 -0700

    Tidy

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ import "alertifyjs/build/css/themes/default.min.css"
 import "alertifyjs/build/css/alertify.min.css"
 import Header from './components/Header'
 
-
 class App extends Component {
 
   state = {
@@ -24,83 +23,60 @@ class App extends Component {
   async retrieveNodeDetails(node)
   {
     try {
-    const nodeInfo = await axios.get(`http://${node.ip}${sysinfo.resource}/?${sysinfo.params.link_info}`)
-
-    if (nodeInfo.status === 200)
-    {   if(nodeInfo.lat !== "") {
+      const nodeInfo = await axios.get(`http://${node.ip}${sysinfo.resource}/?${sysinfo.params.link_info}`)
+      if (nodeInfo.status === 200 && nodeInfo.data.lat && nodeInfo.data.lon) {
         const node = {
-              node: nodeInfo.data.node,
-              lat: nodeInfo.data.lat,
-              lon: nodeInfo.data.lon,
-              meshrf : nodeInfo.data.meshrf,
-              chanbw: nodeInfo.data.chanbw,
-              node_details: nodeInfo.data.node_details,
-              interfaces: nodeInfo.data.interfaces,
-              link_info: Object.keys(nodeInfo.data.link_info).map((key) => nodeInfo.data.link_info[key])
-        }
+          node: nodeInfo.data.node,
+          lat: nodeInfo.data.lat,
+          lon: nodeInfo.data.lon,
+          meshrf : nodeInfo.data.meshrf,
+          chanbw: nodeInfo.data.chanbw,
+          node_details: nodeInfo.data.node_details,
+          interfaces: nodeInfo.data.interfaces,
+          link_info: Object.keys(nodeInfo.data.link_info).map((key) => nodeInfo.data.link_info[key])
+        };
 
-      //  Add this node to the state
-      this.setState({nodesData: [...this.state.nodesData, node]})
-      //console.log("nodesData",this.state.nodesData)
+        //  Add this node to the state
+        this.setState({nodesData: [...this.state.nodesData, node]})
       }
     }
-    else {}
-  }  catch(e) {console.log(`There was an error getting ${node.name} details`)}
+    catch(_) {
+    }
   }
 
   async getNodesData() {
     try {
-    // Get the list of nodes / hosts before to retrieve the nodes information.
-    //const nodes =  await axios.get(`http://${mynode.name}${sysinfo.resource}${sysinfo.params.hosts}`)
-    const nodes =  await axios.get(`http://localnode.local.mesh${sysinfo.resource}${sysinfo.params.hosts}`)
-
-    if(nodes.status !== 200) {
-      //alert("ERROR")
+      // Get the list of nodes / hosts before to retrieve the nodes information.
+      const nodes =  await axios.get(`http://localnode.local.mesh${sysinfo.resource}${sysinfo.params.hosts}`)
+      if(nodes.status !== 200) {
+        alertify.alert("Unable to find your AREDN node, please verify if you are connected to the MESH.");
+      }
+      else {
+        // Get only the ones that matches the format CALLSIGN-CITY-COUNTRY-TYPE#NODENUMBER
+        const regex = new RegExp(this.state.appConfig.nodesFilter);
+        const filteredNodeList = nodes.data.hosts.filter(h => h.name.toUpperCase().trim().match(regex))
+        // Iterate thru each node to get the details.
+        Object.keys(filteredNodeList).forEach(key => this.retrieveNodeDetails(filteredNodeList[key]));
+      }
+    }
+    catch(e) {
       alertify.alert("Unable to find your AREDN node, please verify if you are connected to the MESH.");
     }
-
-    else {
-    // Get only the ones that matches the format CALLSIGN-CITY-COUNTRY-TYPE#NODENUMBER
-    const regex = new RegExp(this.state.appConfig.nodesFilter);
-    //const regex = "[a-zA-z]+[0-9][a-zA-Z]+[-][a-zA-Z]+[-][a-zA-Z]+[-][a-zA-Z]*[1-9]+"
-
-    //const regex = new RegExp("[a-zA-z]");
-
-    console.log(regex,"REGEX")
-    //console.log(this.state.appConfig.nodesFilter, "NODESFILTER")
-    const filteredNodeList = nodes.data.hosts.filter(h => h.name.toUpperCase().trim().match(regex))
-    console.log("Filtered Node List",filteredNodeList)
-
-    // Iterate thru each node to get the details.
-    Object.keys(filteredNodeList).forEach((key) => {
-      this.retrieveNodeDetails(filteredNodeList[key])
-    });
   }
-}
-  catch(e) {
-    alertify.alert("Unable to find your AREDN node, please verify if you are connected to the MESH.");
-  }
-}
 
   async componentDidMount() {
     const appConfig = await axios.get('appConfig.json')
-    //console.log(appConfig.data)
     this.setState({appConfig: appConfig.data})
-    //console.log(appConfig.data,"appConfig.data")
-    //console.log(this.state,"STATE")
     document.title = `${this.state.appConfig.contactInfo.callsign} - ${process.env.REACT_APP_NAME} ${process.env.REACT_APP_VERSION} by ${process.env.REACT_APP_CREATOR}`;
     this.getNodesData();
-    //console.log(this.state.appConfig.app.nodesFilter, "NODES FILTER")
   }
 
   render() {
     return (
-      //<Container fluid>
-        <div>
-          <Header nodesData={this.state.nodesData} appConfig={this.state.appConfig}/>
-          <BaArednMap nodesData={this.state.nodesData} appConfig={this.state.appConfig}/>
-        </div>
-      //</Container>
+      <div>
+        <Header nodesData={this.state.nodesData} appConfig={this.state.appConfig}/>
+        <BaArednMap nodesData={this.state.nodesData} appConfig={this.state.appConfig}/>
+      </div>
     );
   }
 }
