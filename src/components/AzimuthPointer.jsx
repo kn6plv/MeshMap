8095d96872bcb3d4a910eb70cb2b8e2387c90983
CoolAEW <alf.einar.walso@gmail.com>
2024-01-14 01:54:16 +0100

    added azimuthpointer and updated pin popups

---
+++ src/components/AzimuthPointer.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Marker } from "react-leaflet";
+import { Icon } from "leaflet";
+
+const GrayArrow = new Icon({
+    iconUrl: "./mesh_arrow_75px_gray.png",
+    iconSize: [25, 25],
+});
+
+class AzimuthPointer extends React.Component {
+    render() {
+        if (isNaN(this.props.azimuth)) {
+            return "";
+        }
+        return (
+            <Marker
+                position={[this.props.lat, this.props.lon]} // Adjust the position as needed
+                icon={GrayArrow}
+                rotationAngle={parseInt(this.props.azimuth) - 45} // Rotate the arrow icon based on azimuth and correct -45 degrees
+                rotationOrigin="center" // Rotate around the center of the arrow icon
+            />
+        );
+    }
+}
+
+export default AzimuthPointer;
